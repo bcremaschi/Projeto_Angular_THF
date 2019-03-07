548ad920a32f7aa1aebfeb8771c9c75c5a1161b1
Bruno Augusto Da Silva Cremaschi <bruno.cremaschi@totvs.com.br>
2019-03-06 21:19:27 -0300

    Criado componente RESOLVER, para apresentar os cards de leilões conforme integração com a API.

--- src/app/auctions/my-auctions/my-auctions.resolver.ts
+++ src/app/auctions/my-auctions/my-auctions.resolver.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { NewDraftAuctions } from './draft-card/new-draft-autions';
 import { TokenService } from 'src/app/core/token/token.service';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 
